d05f47440ecd8ec95a93874b62ea056a24a07fbb
Christopher Dubeau <dubeauc3@students.cs.macewan.ca>
2016-11-10 16:47:45 -0700

    Edited node_tools.h and node_tools.c and added the deletion of node_tools.cc

--- rapid_onset/node_tools.c
+++ rapid_onset/node_tools.c
@@ -1,4 +1,12 @@
+#include "sysio.h"
+#include "serf.h"
+#include "ser.h"
+#include "plug_null.h"
+#include "tcvphys.h"
+#include "phys_cc1100.h"
 #include "node_tools.h"
+
+#define MAX_P 56
 
 extern int my_id, parent, child;
 
--- rapid_onset/node_tools.h
+++ rapid_onset/node_tools.h
@@ -1,12 +1,3 @@
-#include "sysio.h"
-#include "serf.h"
-#include "ser.h"
-#include "plug_null.h"
-#include "tcvphys.h"
-#include "phys_cc1100.h"
-
-#define MAX_P 56
-
 void set_ids(address packet);
 int get_destination(address packet);
 int get_source_id(address packet);
