a5f8395c9efde2e8a62b7d880ad0bc006f797d1f
Aidan Bush <busha2@cc5-212-19.macewan.ca>
2016-11-10 16:39:29 -0700

    update packet_test.h and rssi_test.h comments on functions

--- rapid_onset/packet_test.h
+++ rapid_onset/packet_test.h
@@ -3,6 +3,12 @@
 
 #include "sysio.h"
 
+/* Records how many on the last 15 packets dropped below the rssi threshold, anh
+ *  returns if too many were lost.
+ * Parameters: the packet address being assessed 
+ * Return: 1 if the number of lost packets in the last 15 packets exceeds a
+ *  thresholds
+ */
 int packet_setup_test(address *);
 
 #endif /*PACKET_TEST_H*/
--- rapid_onset/rssi_test.h
+++ rapid_onset/rssi_test.h
@@ -3,6 +3,11 @@
 
 #include "sysio.h"
 
+/* Records how many on the last 16 packets dropped below the rssi threshold, and
+ *  returns if too many were lost.
+ * Parameters: the packet address being assessed
+ * Return: 1 if the number of low rssi values equals or exceeds threshold
+ */
 int rssi_setup_test(address);
 
 #endif /*RSSI_TEST_H*/
