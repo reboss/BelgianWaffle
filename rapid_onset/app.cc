c6057f173f8fec72e969c567b8f40013f7b22da9
Kevin Saforo <saforok@mymacewan.ca>
2016-11-10 14:24:20 -0700

    Change menu, add header block

--- rapid_onset/app.cc
+++ rapid_onset/app.cc
@@ -1,3 +1,18 @@
+/* ####################################################################
+   CMPT 464
+   Ad Hoc Network Deployment
+   Authors: Aidan Bush, Elliot Sobek, Christopher Dubeau, 
+   John Mulvany-Robbins, Kevin Saforo
+   Thursday, November 10
+
+   File: App.cc
+   Description: The contains the menu, and calls to other functions 
+   for the deployment of an ad hoc network, and the streaming of data
+   through nodes.
+   ####################################################################
+*/
+
+
 #include "sysio.h"
 #include "serf.h"
 #include "ser.h"
@@ -10,6 +25,7 @@ char message[30];
 int my_id = 1;
 int receiver = 0;
 word current_state;
+int ping = 2; //2 Seconds default
 
 fsm transmit {
 
@@ -52,10 +68,11 @@ fsm root {
 	
 	
 	state DISPLAY:
-		ser_outf(DISPLAY, "P2P Chat (Node #%d)\r\n"
-			"(C)hange node ID\r\n"
-			"(D)irect transmission\r\n"
-			"(B)roadcast transmission\r\n"
+		ser_outf(DISPLAY, "Rapid Onset Menu\r\n"
+			"(C)hange Ping Rate\r\n"
+			"(P)acket Deployment\r\n"
+			"(R)SSI Deployment\r\n"
+			"(S)ink Status\r\n"
 			"Selection: ",
 			my_id);
  
@@ -63,20 +80,23 @@ fsm root {
 
 	state SELECTION:
 		switch (selection) {
-		case 'I':
-			ser_inf(SELECTION, "%d", &my_id);
+		case 'C':
+            //TODO: Add ping rate to the ser_out call
+		    ser_outf(SELECTION, "The current ping rate is: %d\r\n", ping);
 			proceed PROMPT;
 			break;
-		case 'D':
-			ser_inf(SELECTION, "%d", &receiver);
-			selection = 'B';
-			proceed PROMPT;
+		case 'P':
+            selection = 'P';
+            proceed PROMPT;
 			break;
-		case 'M':
-			ser_in(SELECTION, message, 30);
-			runfsm transmit;
-			proceed PROMPT;
-			break;
+		case 'R':
+            selection = 'R';
+            proceed PROMPT;
+ 			break;
+       case 'S':
+            selection = 'S';
+            proceed PROMPT;
+             break;
 		default:
 			ser_inf(SELECTION, "%c", &selection);
 			proceed PROMPT;
@@ -88,20 +108,31 @@ fsm root {
 	state PROMPT:
 		switch (selection) {
 		case 'C':
-			ser_outf(PROMPT, "New node ID (1-15): ");
-			selection = 'I';
-			proceed SELECTION;
+		    //TODO: Set ping rate
+		    ser_inf(PROMPT, "%d", &ping);
+			ser_outf(PROMPT, "New ping: %d\r\n", ping);
+			selection = ' ';
+			proceed DISPLAY;
 			break;
-		case 'D':
-			ser_outf(PROMPT, "Receiver node ID (1-15): ");
-			proceed SELECTION;
+		case 'P':
+			ser_out(PROMPT, "Beginning Packet Deployment... \r\n");
+            //TODO: Add Packet Deployment functions
+			selection = ' ';
+			proceed DISPLAY;
 			break;
-		case 'B':
-			ser_outf(PROMPT, "Message : ");
-			selection = 'M';
-			proceed SELECTION;
+		case 'R':
+			ser_out(PROMPT, "Beginning RSSI Deployment... \r\n");
+            //TODO: Add RSSI Deployment functions
+			selection = ' ';
+            proceed DISPLAY;
 			break;
-		default:
+        case 'S':
+            ser_out(PROMPT, "Checking Sink Status... \r\n");
+            //TODO: Do we need this?
+			selection = ' ';
+            proceed DISPLAY;
+            break;
+        default:
 			selection = ' ';
 			proceed DISPLAY;
 			break;
