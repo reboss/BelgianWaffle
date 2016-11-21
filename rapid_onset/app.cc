1a4424e241f820d41b58f8412a2e8325e29e7f9c
Elliott Sobek <elliottsobek@gmail.com>
2016-11-21 09:51:26 -0700

    Fix formatting & add setup behaviour

--- rapid_onset/app.cc
+++ rapid_onset/app.cc
@@ -32,106 +32,106 @@ word current_state;
 int ping = 2;
 
 //Global that indicates if the node is the sink or not
-int sink = 0; 
+int sink = 0;
 
 init_cc1100() {
-  phys_cc1100(0, 60);
-  tcv_plug(0, &plug_null);
-  sfd = tcv_open(WNONE, 0, 0);
+    phys_cc1100(0, 60);
+    tcv_plug(0, &plug_null);
+    sfd = tcv_open(WNONE, 0, 0);
 }
 
 fsm node {
-  state NODE_INIT:
-	if (sink) {
-	  //send setup packet here
-	  ser_out(NODE_INIT, "Setup packet sent\r\n");
+    state NODE_INIT:
+        if (sink) {
+            //send setup packet here
+            ser_out(NODE_INIT, "Setup packet sent\r\n");
 
-	  //send_ping needs to keep retrying
-	  runfsm send_ping;
-	} 
+            //send_ping needs to keep retrying
+            runfsm send_ping;
+        }
 }
 
 fsm root {
 
-	char selection = ' ';
+    char selection = ' ';
 
-	initial state INIT:
-		init_cc1100();
-		runfsm receive;
-		if (sfd < 0) {
-		  runfsm node;
-		  halt();
-		} else {
-		  tcv_control(sfd, PHYSOPT_RXON, NULL);
-		  sink = 1;
-		  proceed DISPLAY;
-		}
+    initial state INIT:
+        init_cc1100();
+        runfsm receive;
+        if (sfd < 0) {
+            runfsm node;
+            halt();
+        } else {
+            tcv_control(sfd, PHYSOPT_RXON, NULL);
+            sink = 1;
+            proceed DISPLAY;
+        }
 
-	state DISPLAY:
-		ser_outf(DISPLAY, "Rapid Onset; Node id (%d)\r\n"
-			"(C)hange Ping Rate\r\n"
-			"(P)acket Deployment\r\n"
-			"(R)SSI Deployment\r\n"
-			"(S)ink Status\r\n"
-			"Selection: ",
-			my_id);
-	        proceed SELECTION;
-	state SELECTION:
-		switch (selection) {
-		case 'C':
-			ser_outf(SELECTION, "The current ping rate is: %d\r\n", ping);
-			proceed PROMPT;
-			break;
-		case 'P':
-			selection = 'P';
-			proceed PROMPT;
-			break;
-		case 'R':
-			selection = 'R';
-			proceed PROMPT;
- 			break;
-		case 'S':
-			selection = 'S';
-			proceed PROMPT;
-			break;
-		default:
-			ser_inf(SELECTION, "%c", &selection);
-			proceed PROMPT;
-			break;
-		}
-		proceed PROMPT;
-		release;
-	state PROMPT:
-		switch (selection) {
-		case 'C':
-			ser_inf(PROMPT, "%d", &ping);
-			ser_outf(PROMPT, "New ping: %d\r\n", ping);
-			selection = ' ';
-			proceed DISPLAY;
-			break;
-		case 'P':
-			ser_out(PROMPT, "Beginning Packet Deployment... \r\n");
-			//TODO: Add Packet Deployment functions
-			// deploy_packet();
-			selection = ' ';
-			proceed DISPLAY;
-			break;
-		case 'R':
-			ser_out(PROMPT, "Beginning RSSI Deployment... \r\n");
-			//TODO: Add RSSI Deployment functions
-			runfsm node;
-			// deploy_rssi();
-			selection = ' ';
-			break;
-		case 'S':
-			ser_out(PROMPT, "Checking Sink Status... \r\n");
-			//TODO: Do we need this?
-			selection = ' ';
-			proceed DISPLAY;
-			break;
-		default:
-			selection = ' ';
-			proceed DISPLAY;
-			break;
-		}
+    state DISPLAY:
+        ser_outf(DISPLAY, "Rapid Onset; Node id (%d)\r\n"
+        "(C)hange Ping Rate\r\n"
+        "(P)acket Deployment\r\n"
+        "(R)SSI Deployment\r\n"
+        "(S)ink Status\r\n"
+        "Selection: ",
+        my_id);
+        proceed SELECTION;
+    state SELECTION:
+        switch (selection) {
+        case 'C':
+            ser_outf(SELECTION, "The current ping rate is: %d\r\n", ping);
+            proceed PROMPT;
+            break;
+        case 'P':
+            selection = 'P';
+            proceed PROMPT;
+            break;
+        case 'R':
+            selection = 'R';
+            proceed PROMPT;
+            break;
+        case 'S':
+            selection = 'S';
+            proceed PROMPT;
+            break;
+        default:
+            ser_inf(SELECTION, "%c", &selection);
+            proceed PROMPT;
+            break;
+        }
+        proceed PROMPT;
+        release;
+    state PROMPT:
+        switch (selection) {
+        case 'C':
+            ser_inf(PROMPT, "%d", &ping);
+            ser_outf(PROMPT, "New ping: %d\r\n", ping);
+            selection = ' ';
+            proceed DISPLAY;
+            break;
+        case 'P':
+            ser_out(PROMPT, "Beginning Packet Deployment... \r\n");
+            //TODO: Add Packet Deployment functions
+            // deploy_packet();
+            selection = ' ';
+            proceed DISPLAY;
+            break;
+        case 'R':
+            ser_out(PROMPT, "Beginning RSSI Deployment... \r\n");
+            //TODO: Add RSSI Deployment functions
+            runfsm node;
+            // deploy_rssi();
+            selection = ' ';
+            break;
+        case 'S':
+            ser_out(PROMPT, "Checking Sink Status... \r\n");
+            //TODO: Do we need this?
+            selection = ' ';
+            proceed DISPLAY;
+            break;
+        default:
+            selection = ' ';
+            proceed DISPLAY;
+            break;
+        }
 }
