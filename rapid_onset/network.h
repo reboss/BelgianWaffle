e5b4a1d0d018de3bf9ea261b3e07c530b94a462f
Elliott Sobek <sobeke@cc5-212-11.macewan.ca>
2016-11-25 14:31:56 -0700

    Add header guards & header blocks

--- rapid_onset/network.h
+++ rapid_onset/network.h
@@ -1,3 +1,18 @@
+/* ####################################################################
+   CMPT 464
+   Ad Hoc Network Deployment
+   Authors: Aidan Bush, Elliott Sobek, Christopher Dubeau,
+   John Mulvany-Robbins, Kevin Saforo
+   Thursday, November 10
+
+   File: node_tools.h
+   Description: The header file for node_tools.c
+   ####################################################################
+*/
+
+#ifndef NETWORK_H
+#define NETWORK_H
+
 //+++ "network.cc"
 
 void receive(word __pi_st);
@@ -5,3 +20,5 @@ void receive(word __pi_st);
 void send_ping(word __pi_st);
 
 void send_deploy(word __pi_st);
+
+#endif /* End NETWORK_H */
--- rapid_onset/node_led.h
+++ rapid_onset/node_led.h
@@ -1,3 +1,20 @@
+/* ####################################################################
+   CMPT 464
+   Ad Hoc Network Deployment
+   Authors: Aidan Bush, Elliott Sobek, Christopher Dubeau,
+   John Mulvany-Robbins, Kevin Saforo
+   Thursday, November 10
+
+   File: node_tools.h
+   Description: The header file for node_tools.c
+   ####################################################################
+*/
+
+#ifndef NODE_LED_H
+#define NODE_LED_H
+
 //+++ "node_led.cc"
 
 void node_leds(word __pi_st);
+
+#endif /* End NODE_LED_H */
