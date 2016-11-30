d3d28d99a48e17a6395cc7800f0ad5ad8912960a
Elliott Sobek <sobeke@cc5-212-17.macewan.ca>
2016-11-30 00:29:22 -0700

    Inline defines from .cc file

--- rapid_onset/network.h
+++ rapid_onset/network.h
@@ -17,9 +17,6 @@
 
 #define MAP_P 56
 
-#define RSSI_TEST   1
-#define PACKET_TEST 2
-
 #define PING_LEN     10
 #define STOP_LEN     10
 #define ACK_LEN      10
@@ -34,8 +31,16 @@
 #define ACK      5
 #define DEPLOYED 6
 #define STOP     7
+#define KILL     8
 
 #define DONE diag("\r\nDone\r\n")
+
+#define LED_YELLOW 0
+#define LED_GREEN  1
+#define LED_RED    2
+#define LED_RED_S  3
+
+#define SINK_ID 0
 
 void receive(word __pi_st);
 
