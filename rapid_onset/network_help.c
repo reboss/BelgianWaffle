311697c7b0ab968efcf0f4ef28eb8b4ca3d1ba71
Aidan Bush <busha2@cc5-212-17.macewan.ca>
2016-12-04 11:46:27 -0700

    add network tracking struct

--- rapid_onset/network_help.c
+++ rapid_onset/network_help.c
@@ -14,8 +14,22 @@
 #include "tcv.h"
 #include "network_help.h"
 
+extern stream_stat stream_info;
+
 void set_power(int sfd, int power) {
     int pow = power;
     tcv_control(sfd, PHYSOPT_SETPOWER, &power);
+}
+
+void add_stream_info(address packet, int i) {
+    if (i >= NUM_TEST_MSGS) {
+        if (debug >= 1)
+            diag("Past max msgs for test data\r\n");
+        return;
+    }
+    stream_info.packet_loss[i] = ;
+    stream_info.seq_num[i] = get_seqnum(packet);
+    stream_info.packet_len[i] = get_length(packet);
+    stream_info.timestamp[i] = seconds();
 }
 
